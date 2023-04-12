d20d5485d05dde60f6961b76d971b912287defc5
Sergey <matuinin@yahoo.com>
2023-04-12 21:46:30 +0300

    Home page uses ads now

--- src/Web/WebMVC/Controllers/HomeController.cs
+++ src/Web/WebMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using MssDevLab.Common.Models;
 using MssDevLab.WebMVC.Models;
 using MssDevLab.WebMVC.Services;
+using MssDevLab.WebMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -27,13 +28,15 @@ namespace MssDevLab.WebMVC.Controllers
             var serviceRequest = new ServiceRequest
             {
                 PageNumber = 1,
-                PageSize = 10,
+                PageSize = 5,
                 QueryString = string.Empty,
                 UserPreferences = null
             };
             var adsResponse = await _testAdService.FetchAds(serviceRequest);
             _logger.LogInformation("HomeController gets items from test ad service. Items:{itemsCount}", adsResponse?.Items?.Count());
-            return View();
+            
+            var viewModel = new HomeIndexViewModel(adsResponse?.Items);
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
---
+++ src/Web/WebMVC/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MssDevLab.Common.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace MssDevLab.WebMVC.ViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public HomeIndexViewModel(IEnumerable<ServiceData>? commonAds) 
+        { 
+            CommonAds = commonAds;
+        }
+
+        public IEnumerable<ServiceData>? CommonAds { get; set; }  
+
+        public bool HasCommonAds => CommonAds != null && CommonAds.Any();
+    }
+}
